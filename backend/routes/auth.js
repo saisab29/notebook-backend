3ef4b632de37bb7a0dedd1be4984636f5560a873
saisab29 <107755209+saisab29@users.noreply.github.com>
2022-11-23 17:55:04 +0545

    Added hash password generation and session token using jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const { response } = require('express');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "whatismeasurement";
 
 
 
@@ -31,13 +34,26 @@ router.post('/createUser', [
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
 
         }
+
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+
+        //Create a new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
-        })
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
 
-        res.json(user);
+        res.json({ authtoken });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured");
