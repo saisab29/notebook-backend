b6df55912ca919aae4c61db42286a0a79657cd36
saisab29 <107755209+saisab29@users.noreply.github.com>
2022-11-23 18:57:35 +0545

    Created login endpoint by sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,5 +64,46 @@ router.post('/createUser', [
 
 })
 
+//Authenticate a User using: POST "/api/auth/login", No loging required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+
+], async (req, res) => {
+    //If there are errors, return BAd request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please login with correct credentials" })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please login with correct credentials" })
+
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        res.json({ authtoken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+
+    }
+
+
+
+})
 
 module.exports = router
