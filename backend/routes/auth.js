c10ac200d3f9b8fd5e0f04b4c82e3ecef9315442
saisab29 <107755209+saisab29@users.noreply.github.com>
2022-11-24 19:14:27 +0545

    Created a middleware to fetchuser data from jwt auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = "whatismeasurement";
 
 
 
-//Create a User using: POST "/api/auth/createuser". Doesnt require Auth
+//ROUTE 1:  Create a User using: POST "/api/auth/createuser". Doesnt require Auth
 
 
 
@@ -64,7 +65,8 @@ router.post('/createUser', [
 
 })
 
-//Authenticate a User using: POST "/api/auth/login", No loging required
+
+//ROUTE 2: Authenticate a User using: POST "/api/auth/login", No loging required
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists(),
@@ -105,5 +107,22 @@ router.post('/login', [
 
 
 })
+
+
+
+//ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser",  loging required
+router.post('/getuser', fetchuser, async (req, res) => {
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password")
+        res.send(user)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+
+    }
+})
+
 
 module.exports = router
