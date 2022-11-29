daaea4ad1a7bb3ba8157c17710ab48022983f889
saisab29 <107755209+saisab29@users.noreply.github.com>
2022-11-29 21:40:12 +0545

    Created Context hook for notes

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,24 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
 
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/' element={<About />} />
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
 
-        </Routes>
-      </Router>
-
-
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+
+const About = () => {
+
+
+    return (
+        <div>This is about</div>
+    )
+}
+
+export default About
