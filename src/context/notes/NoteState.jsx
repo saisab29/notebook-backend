5d553d36679dbda598efd34d8d7c630b3631e4d4
saisab29 <107755209+saisab29@users.noreply.github.com>
2022-12-02 19:41:16 +0545

    Created functionality to delete a note

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -82,12 +82,16 @@ const NoteState = (props) => {
 
     }
     //Delete a note
-    const deleteNote = () => {
+    const deleteNote = (id) => {
+        //TODO: API call
+        console.log("Deleting the note with id" + id);
+        const newNotes = notes.filter((note) => { return note._id !== id })
+        setNotes(newNotes)
 
     }
 
     //Edit a note
-    const editNote = () => {
+    const editNote = (id, title, description, tag) => {
 
     }
 
